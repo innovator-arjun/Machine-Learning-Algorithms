39213446d4284f8e482e2382813ec077d5a74d91
innovator_arjun <vsarjun412@gmail.com>
2018-10-04 10:53:03 +0530

    Version 1.9.8

--- NLP/NLP.py
+++ NLP/NLP.py
@@ -12,4 +12,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #importing the dataset
+#tsv means tab seperated value
+#we Should use tsv for NLP. Since in text people will use comma.
+data_review=pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t',quoting=3)
+
+#Cleaning the tests like adjective, preprosition and article, punchuation,
+#Stemming will make the word into root word.
+#loves,love,loved,loving -->stemming
+
+
+#Step 1: 
+import re
+
+#To remove everything except a-z , A-Z and space.
+review=re.sub('[^a-zA-Z]',' ',data_review['Review'][0])
 
