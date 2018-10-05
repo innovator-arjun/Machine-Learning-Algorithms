fc2eb0c7b7b9c413dcda0ce8a9484a9c23b1bc39
innovator_arjun <vsarjun412@gmail.com>
2018-10-05 09:20:10 +0530

    Version 1.9.9

--- NLP/NLP.py
+++ NLP/NLP.py
@@ -16,9 +16,6 @@ import matplotlib.pyplot as plt
 #we Should use tsv for NLP. Since in text people will use comma.
 data_review=pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t',quoting=3)
 
-#Cleaning the tests like adjective, preprosition and article, punchuation,
-#Stemming will make the word into root word.
-#loves,love,loved,loving -->stemming
 
 
 #Step 1: 
@@ -27,3 +24,24 @@ import re
 #To remove everything except a-z , A-Z and space.
 review=re.sub('[^a-zA-Z]',' ',data_review['Review'][0])
 
+#Step 2:
+
+#to make the text to lower case
+review=review.lower()
+
+
+#Cleaning the tests like adjective, preprosition and article, punchuation,
+#Stemming will make the word into root word.
+#loves,love,loved,loving -->stemming
+
+#Step 3:
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+#to make the string to list of strings
+review=review.split()
+
+#for loop to remove the stop words from our review
+#use set() to commpute fastter
+review=[word for word in review if not word in set(stopwords.words('english'))]
