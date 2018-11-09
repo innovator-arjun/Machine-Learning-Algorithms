70febece722d9836a4071e4a4246d5d3f0a21317
innovator_arjun <vsarjun412@gmail.com>
2018-11-09 10:21:05 +0530

    Version 2.2.4

--- Dimensionality Reduction/PCA.py
+++ Dimensionality Reduction/PCA.py
@@ -48,6 +48,10 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
+X_train
+
+y_train
+
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
